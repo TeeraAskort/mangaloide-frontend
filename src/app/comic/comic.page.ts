3fa25606af3b971501479d087b7ccbc51b2213c0
Alderaeney <alderaeney@gmail.com>
2022-04-06 19:23:36 +0200

    Added getting the route parameter in comic view

--- src/app/comic/comic.page.ts
+++ src/app/comic/comic.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-comic',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comic.page.scss'],
 })
 export class ComicPage implements OnInit {
+  public id: number | undefined = undefined;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    const param: string | null = route.snapshot.paramMap.get('id');
+    if (param) {
+      this.id = parseInt(param, 10);
+    } else {
+      router.navigate(['/home']);
+    }
   }
 
+  ngOnInit() {}
 }
